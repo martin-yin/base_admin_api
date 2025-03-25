76ed5d0cbb1706f362f6c0dbea9ff603510f06e3
尹县伟 <1605074969@qq.com>
2025-03-25 11:13:00 +0800

    wip: 重构项目

--- src/shared/exceptions/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './api.exception';
-export * from './unauthorized.exception';
--- src/shared/exceptions/unauthorized.exception.ts
+++
@@ -1,7 +0,0 @@
-import { UnauthorizedException } from '@nestjs/common';
-
-export class UnauthorizedError extends UnauthorizedException {
-  constructor(message?: string, error?: any) {
-    super(message || '权限验证失败', error);
-  }
-}
