b4d466c1792aa5f57e5113f4229014626a3bb6b1
尹县伟 <1605074969@qq.com>
2025-03-26 16:14:00 +0800

    wip: 重构项目

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,8 +3,6 @@ import { SystemModule } from './system/system.module';
 import { UploadModule } from './upload/upload.module';
 import { UserModule } from './user/user.module';
 import { APP_GUARD } from '@nestjs/core';
-
-// import { PermissionGuard } from '@/shared/auth/guards/permission.guard';
 import { AdminAuthModule } from '@/shared/auth/admin-auth.module';
 import { AdminJwtAuthGuard } from '@/shared/auth/guards/admin-auth.guard';
 import { PermissionGuard } from '@/shared/auth/guards/permission.guard';
